8fa22514e9f1652d5d8aab5104469b6c84a4fcf1
Marta Mendel <114557943+Qniara@users.noreply.github.com>
2024-01-08 18:01:03 +0100

    Update Zad_08_01_2024.sql

--- Zadania/Zad_08_01_2024.sql
+++ Zadania/Zad_08_01_2024.sql
@@ -1,1 +1,7 @@
 (ROW_NUMBER() OVER(ORDER BY IDstudenta)+100), - licznik
+INSERT INTO studenci2 SELECT pracownicy.ImiePracownika, pracownicy.NazwiskoPracownika, pracownicy.AdresPracownika, pracownicy.MiastoPracownika, pracownicy.StanZamPracownika, pracownicy.KodPocztowyPracownika, 
+pracownicy.NumKierPracownika, pracownicy.TelefonPracownika, '1991-03-18', 'M', NULL , 2 from pracownicy INNER JOIN wykladowcy ON pracownicy.IDPracownika=wykladowcy.IDPracownika WHERE wykladowcy.Tytul='Profesor';
+
+ALTER TABLE studenci2 ADD IDStudenta2 INT PRIMARY KEY AUTO_INCREMENT;
+
+INSERT INTO studenci2 SELECT null, ImieStudenta, na
