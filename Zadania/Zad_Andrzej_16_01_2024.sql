34b95ec5cfd1430e92427fa041d7e78480d0e899
Marta Mendel <114557943+Qniara@users.noreply.github.com>
2024-01-16 08:28:00 +0100

    Update Zad_Andrzej_16_01_2024.sql

--- Zadania/Zad_Andrzej_16_01_2024.sql
+++ Zadania/Zad_Andrzej_16_01_2024.sql
@@ -1,1 +1,4 @@
 /*Wyswietl wszystkie zamówienia wysłane 3 października 2017 roku i nazwiska klientow  którzy je zalożyli*/
+Select NumerZamowienia, DataWysylki, klienci.NazwiskoKlienta 
+from zamowienia JOIN klienci ON zamowienia.IDKlienta=klienci.IDKlienta 
+where zamowienia.DataWysylki='2017-10-03';
