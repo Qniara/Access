84db6ad744a2058d50170d15edab9054603caa2e
Marta Mendel <114557943+Qniara@users.noreply.github.com>
2024-01-16 09:34:50 +0100

    Update Zad_Andrzej_16_01_2024.sql

--- Zadania/Zad_Andrzej_16_01_2024.sql
+++ Zadania/Zad_Andrzej_16_01_2024.sql
@@ -6,3 +6,9 @@ where zamowienia.DataWysylki='2017-10-03';
 select klienci.ImieKlienta, klienci.NazwiskoKlienta, 
   (select max(DatazZamowienia) from zamowienia where zamowienia.IDKlienta=klienci.IDKlienta) as dataostzam 
 from klienci;
+
+/*Zrob liste klientow i wszystkich szczegółów dotyczących ich ost zamowienia*/
+select ImieKlienta, NazwiskoKlienta, 
+  (select NumerZamowienia, zamowienia.DataZamowienia, zamowienia.DataWysylki 
+  from zamowienia where zamowienia.IDKlienta=klienci.IDKlienta) 
+from klienci;
