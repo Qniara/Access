532dc17ce789446a8adb5179e244cb41f958a430
Marta Mendel <114557943+Qniara@users.noreply.github.com>
2024-01-30 09:09:58 +0100

    Update Zad_30_01_2024.sql

--- Zadania/Zad_30_01_2024.sql
+++ Zadania/Zad_30_01_2024.sql
@@ -1,4 +1,11 @@
-/*Zad.1*/
+/*Zad.1
+Wymien skladniki nie uzywane  wzadnym przepisie*/
 select skladniki.NazwaSkladnika, skladniki_przepisow.IDMiaryIlosci 
 from skladniki left join skladniki_przepisow on skladniki.IDSkladnika=skladniki_przepisow.IDSkladnika 
 where skladniki_przepisow.IDMiaryIlosci=null;
+
+/*Zad2
+wyswietl agentow ktorzy dotychczas nie podpisali kontraktu z zadnym wykonawca (nie maja zadnej imprezy)*/
+select agenci.ImieAgenta, agenci.NazwiskoAgenta 
+from agenci left outer join imprezy on agenci.IDAgenta=imprezy.IDAgenta 
+where imprezy.NumerImprezy is null;
