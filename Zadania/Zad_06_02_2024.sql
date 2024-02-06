3d2429fdf0710cc09dcc4478a1b57fe25e1355d5
Marta Mendel <114557943+Qniara@users.noreply.github.com>
2024-02-06 09:19:21 +0100

    Update Zad_06_02_2024.sql

--- Zadania/Zad_06_02_2024.sql
+++ Zadania/Zad_06_02_2024.sql
@@ -1,3 +1,4 @@
+/*KARTKOWKA*/
 /*Zad.1*/
 select wykonawcy.NazwaScenicznaWykonawcy, imprezy.DzienRozpoczecia, imprezy.DzienZakonczenia, imprezy.WartoscKontraktu 
 from wykonawcy join imprezy on wykonawcy.IDWykonawcy=imprezy.IDWykonawcy 
@@ -9,3 +10,12 @@ order by imprezy.WartoscKontraktu desc
 select przedmioty.NazwaPrzedmiotu 
 from przedmioty inner join zajecia on przedmioty.IDPrzedmiotu=zajecia.IDPrzedmiotu 
 where zajecia.PlanNaSrode=1
+
+/*ZAD Z LEKCJI*/
+select agenci.IDAgenta, agenci.ImieAgenta, 
+case 
+	when IDAgenta<30 then 'male id'
+    when IDAgenta>30 then 'duze id'
+    else 'id rowne 30'
+end as nazwa
+from agenci 
