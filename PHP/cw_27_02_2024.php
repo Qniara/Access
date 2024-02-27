a3c3cbdfc0003e7ac5dc6d8945d3cfd3392d5e3a
Marta Mendel <114557943+Qniara@users.noreply.github.com>
2024-02-27 09:20:58 +0100

    Update cw_27_02_2024.php

--- PHP/cw_27_02_2024.php
+++ PHP/cw_27_02_2024.php
@@ -3,8 +3,13 @@ echo "Vodka? Yes I love vodka."."<br"."br";
 $serwer="localhost";
 $user="root";
 $haslo="";
-$baza="ptaki"
+$baza="ptaki";
 $zapytanie="SELECT gatunki.nazwa_zwyczajowa FROM gatunki WHERE gatunki.nazwa_zwyczajowa LIKE 'b%' ORDER By gatunki.nazwa_zwyczajowa ASC";
 $con = new mysqli($serwer,$user,$haslo,$baza);
 if($wynik=$con->query($zapytanie))
+    while($row=$wynik->fetch_array())
+        echo $row["nazwa_zwyczajowa"]."<br>";
+else
+    echo $con->errno." ".$con->error;
+$con->close();
 ?>
