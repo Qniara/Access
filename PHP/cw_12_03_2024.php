fd3bec087fc705474581826f06729fe0a1e784ed
Marta Mendel <114557943+Qniara@users.noreply.github.com>
2024-03-12 09:14:47 +0100

    Update cw_12_03_2024.php

--- PHP/cw_12_03_2024.php
+++ PHP/cw_12_03_2024.php
@@ -1,1 +1,14 @@
-
+<?PHP
+$serwer ="localhost";
+$user ="root";
+$haslo ="";
+$baza ="ksiegarnia";
+$zapytanie ="SELECT imie, nazwisko, miejscowosc FROM klienci";
+$con = new mysqli($serwer,$user,$haslo,$baza);
+if($wynik=$con->query($zapytanie))
+   WHILE($row=$wynik->fetch_array())
+       echo $row["imie"]." ".$row["nazwisko"]." ".$row["miejscowosc"]."<br>";
+else
+   echo $con->errno." ".$con->error; 
+$con->close();
+?>
