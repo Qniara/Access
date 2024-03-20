c58982fd4b12ca1c3fd1f83ccd25bf7bf8c6aaf4
Marta Mendel <114557943+Qniara@users.noreply.github.com>
2024-03-20 12:06:28 +0100

    Update cw_20_03_2024.php

--- PHP/Barski/cw_20_03_2024.php
+++ PHP/Barski/cw_20_03_2024.php
@@ -1,5 +1,29 @@
-//127.0.0.1 (localhost) - do xamppa w przeglądarce
 <?php
-echo "dupa";
-echo "<body bgcolor='pink'><br><br><b><i>kibel</i></b></body>";
+echo"<body bgcolor='pink'>";
+
+// echo "dupa";
+// echo "<body bgcolor='pink'><br><br><b><i>kibel<br><br></i></b></body>";
+// print("jestem gejem<br><br>");
+// phpinfo();
+
+// $imie="Ziemniak";
+// $nazwisko="Pieczony";
+// print("Nazywam siem ".$imie." ".$nazwisko);
+
+// $a =5;
+// $b = 7;
+// print("Pole prostokata o bokach ". $a ." i ". $b ." wynosi ".$a*$b);
+// print("<br>Obwod prostokata o bokach <i>". $a ."</i> i <i>". $b ."</i> wynosi <i>".$a*2+$b*2);
+
+// define("KWOTA_DO_ODJECIA",436.20);
+// define("STAWKA_PODATKOWA",0.19);
+// print("Podatek od dochodu 5000 PLN w 2000 roku wynosi: ");
+// printf("%0.2f",(5000*STAWKA_PODATKOWA - KWOTA_DO_ODJECIA));
+// print('PLN');
+
+// $tekst = 'Kurt ';
+// $tekst .= 'Cobain';
+// $tekst .= '<BR>';
+// $tekst .= 'Seattle';
+// print($tekst);
 ?>
